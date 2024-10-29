4abb9dfad42d05d866992575327f4e6c2d9483ba
jaman7 <krzysztofbalon123@gmail.com>
2024-10-29 15:27:27 +0100

    init

--- src/app/core/http/http.utils.ts
+++ src/app/core/http/http.utils.ts
@@ -1,16 +1,9 @@
 import { HttpParams } from '@angular/common/http';
-import moment from 'moment';
 import { IParams } from './http.model';
 
 export function preparedHttpParamsValue(value: any): string {
   if (value instanceof String) {
     return value.toString();
-  }
-  if (moment.isMoment(value)) {
-    return value.format('DD-MM-YYYY');
-  }
-  if (value instanceof Date) {
-    return moment(value).format('DD-MM-YYYY');
   }
   if (Array.isArray(value)) {
     return value.join('&');
